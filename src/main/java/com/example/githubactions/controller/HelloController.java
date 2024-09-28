3d12ae83e505013a2809b582c159a40d0078083a
Antonio Demarchi <56514877+DemarchiWorking@users.noreply.github.com>
2024-09-28 01:05:18 -0300

    feature/altera-texto

--- src/main/java/com/example/githubactions/controller/HelloController.java
+++ src/main/java/com/example/githubactions/controller/HelloController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloController {
     @GetMapping("/hello")
-    public String hello() {
-        return "Olá mundo";
+    public String olamundo() {
+        return "Olá mundo, test build";
     }
 }
